2ec17fecfe73ec7038d62be6ccab163ce9b7c5a9
ducta-qc <duc@chappiebot.com>
2018-06-01 09:56:24 +0700

    Remove test_neptune.py

--- adapters/neptune.py
+++ adapters/neptune.py
@@ -2,7 +2,41 @@ from __future__ import print_function  # Python 2/3 compatibility
 from gremlin_python.structure.graph import Graph
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 
-
+'''
+Vertices:
+    "CarMaker"
+    "Organization" 
+    "Definition"
+    "People"
+    "Sale"
+    "CarModel"
+    "CarSeries"
+    "CarTransmission"
+    "CarEngine"
+    "Color"
+    "CarTire"
+    "CarWheel"
+    "Place"
+    "CarDoor"
+    "CarSeat"
+    "CarBody"
+    "CarSuspensions"
+    "CarAirConditioner"
+    "CarEntertainment"
+    "CarLight"
+    "CarDrivingAssistant"
+    "CarSafety"
+    "CarFuelConsumption"
+    "CarPrice"
+Edges:
+    "owned_by"
+    "parent_of"
+    "found_by",
+    "attr"
+    "made_by"
+    "made_in"
+    "similar"
+'''
 class Neptune(object):
     def __init__(self, uri):
         """
